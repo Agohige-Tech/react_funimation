530a1eb0e3e640aa2ddd7fef28d660550257130d
Porter <crporter171@gmail.com>
2021-12-08 17:00:59 -0500

    Start of my Funimation clone

---
+++ src/Banner.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <div className="bannerMessage">
+        <div className="bannerMessageDescription">
+          <h1>If it's Anime, it's Funimation.</h1>
+          <p>
+            Watch over 15,000 hours of anime anywhere, ad-free. Only thing
+            missing are the snacks.
+          </p>
+        </div>
+        <button>WATCH NOW</button>
+      </div>
+      <div className="bannerImage">
+        <img src="" />
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ src/Header.js
@@ -0,0 +1,71 @@
+import React from "react";
+import "./Header.css";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="promo">
+        <p>
+          DEMON SLAYER: KIMETSU NO YAIBA - ENTERTAINMENT DISTRICT ARC - START
+          WATCHING WITH A 14 DAY FREE TRIAL
+        </p>
+      </div>
+      <div className="nav_container">
+        <nav className="nav">
+          <div className="logo">
+            <img
+              src="https://static.funimation.com/static/img/common/funimation-logo.svg"
+              alt="funimation logo"
+            />
+          </div>
+          <ul className="dropDown">
+            <li className="stream">
+              <p>Stream</p>
+              <ul className="nav_content">
+                <li>All Titles</li>
+                <li>Current Season</li>
+                <li>Schedule</li>
+              </ul>
+            </li>
+            <li>
+              <p className="discover">Discover</p>
+              <ul className="nav_content">
+                <li>Games</li>
+                <li>News</li>
+                <li>Theatrical</li>
+              </ul>
+            </li>
+            <li>
+              <p className="Shop">Shop</p>
+              <ul className="nav_content">
+                <li>Home Video</li>
+                <li>Pre-Orders</li>
+                <li>Figures & Collectables</li>
+                <li>Accessories</li>
+                <li>Apparel</li>
+                <li>Home Goods</li>
+                <li>Gift Cards</li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <div className="headerRight">
+          <div className="promoBtn">
+            <p>Try Premium Plus</p>
+          </div>
+          <div className="loginContainer">
+            <ul>
+              <li className="login">Log in</li>
+              <li>
+                <SearchOutlined />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
